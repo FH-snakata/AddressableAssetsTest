a049a3e0db4e23c519c7fb063479b00baf253bd8
snakata <majime.nekodanshi@gmail.com>
2018-09-02 23:30:59 +0900

    登録した画像を読み込んで表示するスクリプトを追加

---
+++ Assets/Scripts/LoadSprite.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+
+public class LoadSprite : MonoBehaviour
+{
+    [SerializeField] private Image _image = null;
+
+    private void Start()
+    {
+        Addressables.LoadAsset<Sprite>("Assets/Textures/icon_medical/icon_medical_man01.png").Completed +=
+            op => _image.sprite = op.Result;
+    }
+}
---
+++ Assets/Scripts/LoadSprite2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+
+public class LoadSprite2 : MonoBehaviour
+{
+    [AssetReferenceTypeRestriction(typeof(Texture2D))]
+    public AssetReference AssetReference;
+
+    [SerializeField] private Image _image = null;
+
+    private void Start()
+    {
+        Addressables.LoadAsset<Sprite>(AssetReference).Completed += op => _image.sprite = op.Result;
+    }
+}
